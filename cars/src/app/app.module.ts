fa5279a499c84715063253fc957c6e90fecd0106
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-08-25 15:45:47 -0700

    Adding CRUD

--- cars/src/app/app.module.ts
+++ cars/src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cars/src/app/car.service.ts
+++ cars/src/app/car.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
 import { Car } from './car';
@@ -12,12 +12,35 @@ export class CarService {
   baseUrl = 'http://localhost:8888/api';
 
   constructor(private http: HttpClient) { }
-
+  // get all cars in db
   getAll() {
     return this.http.get(`${this.baseUrl}/list`).pipe(
       map((res: any) => {
         return res['data'];
       })
     );
-  }
-}
+  } // getAll
+
+  // put car into db
+  store(car: Car) {
+    return this.http.post(`${this.baseUrl}/store`, {data: car}).pipe(
+      map((res: any) => {
+        return res['data'];
+      })
+    );// return
+  } // store()
+
+  // update car in db
+  update(car: Car) {
+    return this.http.put(`${this.baseUrl}/update`, { data: car });
+  } //update
+  
+  delete(id: any) {
+    const params = new HttpParams()
+      .set('id', id.toString());
+
+    return this.http.delete(`${this.baseUrl}/delete`, { params: params });
+  } // delete
+
+
+} // CarService
